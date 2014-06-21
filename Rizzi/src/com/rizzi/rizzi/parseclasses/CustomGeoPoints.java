69f5c70aa0aa650660b32c3e5d3624652a9dc432
David Zeng <naoh.zeng@gmail.com>
2014-06-21 12:00:41 -0500

    Added a customGeopoint class in the parse data model framework, which is just a wrapper class for a Parse geopoin class. So this way, other parse classes can have more than 2 geopoint classes in their class.

---
+++ Rizzi/src/com/rizzi/rizzi/parseclasses/CustomGeoPoints.java
@@ -0,0 +1,23 @@
+package com.rizzi.rizzi.parseclasses;
+
+import com.parse.ParseClassName;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+
+@ParseClassName("CustomGeoPoints")
+public class CustomGeoPoints extends ParseObject {
+	
+	
+	public static final String KEY_PARSE_GEO_POINT = "ParseGeoPoint";
+
+	public ParseGeoPoint getGeoPoint(){
+		return getParseGeoPoint(KEY_PARSE_GEO_POINT);
+	}
+	
+	public void setGeoPoint(ParseGeoPoint point){
+		put(KEY_PARSE_GEO_POINT, point);
+	}
+	
+	
+	
+}
